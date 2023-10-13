b2fa284a2c70bf98076032657912b75fe71d5b3f
pavithras2310 <pavithraselvam2310@gmail.com>
2023-10-13 21:52:00 +0530

    app js file added

---
+++ resume-task/app.js
@@ -0,0 +1,18 @@
+document.getElementById("stuName").innerHTML="Pavithra S"
+document.getElementById("rolename").innerHTML="Full Stack Developer"
+document.getElementById("aboutmePara").innerText="Passionate and motivated Software engineer seeking to establish a successful career in the software development field.Possessing a solid foundation in programming, problem-solving,and a keen interest in developing innovative solutions. Eager to contribute my skills and learn from experienced professionals to make a positive impact on the industry."
+document.getElementById("aboutmePara").style="line-height:25px"
+document.getElementById("qualification1").innerHTML="<tr><td>1</td><td>BE Computer Science</td><td>2021</td></tr>"
+document.getElementById("qualification2").innerHTML="<tr><td>2</td><td>HSC</td><td>2017</td></tr>"
+document.getElementById("qualification3").innerHTML="<tr><td>3</td><td>SSLC</td><td>2015</td></tr>"
+document.getElementById("certificationpara").innerHTML="<li>Tech Foundation for Web Development Certification</li><li> KGiSL Micro College for Full Stack Development Certification</li>"
+document.getElementById("certificationpara").style="line-height:25px"
+document.getElementById("internshipPara").innerHTML="Full Stack Developer <br>September - Present<br>KGiSL Micro College. Coimbatore"
+document.getElementById("internshipPara").style="line-height:25px"
+document.getElementById("internship").className="internshipClass"
+console.log("Hi This is my resume");
+window.alert("Welcome to my RESUME!");
+window.prompt("Please enter your name to login to see my resume")
+
+
+
